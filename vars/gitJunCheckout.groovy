2034eaab5c15d86ce56607ff2bb6c9da7a686892
sutthiphong2005 <sutthiphong2005@gmail.com>
2025-08-11 20:29:29 +0700

    Update and rename gitJunCheckout to gitJunCheckout.groovy

---
+++ vars/gitJunCheckout.groovy
@@ -0,0 +1,6 @@
+def call(Map config=[:]) {
+  checkout scmGit(branches: [[name: '*/master']], 
+                  extensions: [], 
+                  userRemoteConfigs: [[url: 'https://github.com/jenkins-docs/simple-node-js-react-npm-app.git']])
+
+}
